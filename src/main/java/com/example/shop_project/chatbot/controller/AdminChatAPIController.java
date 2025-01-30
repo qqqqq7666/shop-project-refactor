d2add3b6c1ab5d013764ea183c6b12841090c825
rudcjs <rudcjs335@gmail.com>
2025-01-30 22:10:01 +0900

    Test commit

--- src/main/java/com/example/shop_project/chatbot/controller/AdminChatAPIController.java
+++ src/main/java/com/example/shop_project/chatbot/controller/AdminChatAPIController.java
@@ -1,15 +1,9 @@
 package com.example.shop_project.chatbot.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.shop_project.chatbot.service.ChatService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/chat/admin")
